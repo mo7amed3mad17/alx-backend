4eb6f3956cd16fe5d39c171893977b5a4e7206b7
Mohamed Emad <memo12317emad@gmail.com>
2024-10-28 20:27:34 +0200

    simple_helper

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+""" Simple Helper Function"""
+
 def index_range(page: int, page_size: int) -> tuple:
     start_index = (page) * page_size
     end_index = start_index + page_size
