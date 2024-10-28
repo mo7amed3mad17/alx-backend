6da0a5db19d65c9856a022aece172cc67bae929b
Mohamed Emad <memo12317emad@gmail.com>
2024-10-28 20:30:36 +0200

    simple_helper

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,6 +3,7 @@
 
 
 def index_range(page: int, page_size: int) -> tuple:
+    """ A function that return the statrt and end indeces of the data """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
